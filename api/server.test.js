bb7e1be1441031e1828c9f12e213eb713ea4f872
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-26 12:41:30 -0400

    wrote first test for post /register endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,24 @@
+const db = require('../data/dbConfig');
+const request = require('supertest');
+const authRouter = require('./auth/auth-router');
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(false)
+})
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+
+beforeEach(async () => {
+  await db.seed.run();
+})
+
+describe('[POST] /register', () => {
+  test('responds with status 201', async () => {
+    const res = await request(authRouter).post('/register');
+    expect(res.status).toBe(201);
+  })
 })
