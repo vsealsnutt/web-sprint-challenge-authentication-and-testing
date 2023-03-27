ae30a710914af5724cea336902515dcbc537bd75
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-27 12:58:36 -0400

    wrote tests for first post endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,10 +1,10 @@
 const db = require('../data/dbConfig');
 const request = require('supertest');
-const authRouter = require('./auth/auth-router');
+const server = require('./server');
 
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
 
 beforeAll(async () => {
@@ -18,7 +18,14 @@ beforeEach(async () => {
 
 describe('[POST] /register', () => {
   test('responds with status 201', async () => {
-    const res = await request(authRouter).post('/register');
+    const res = await request(server).post('api/auth/register');
     expect(res.status).toBe(201);
+  })
+  test('adds new user to users table on success', async () => {
+    await (await request(server).post('/api/auth/register')).send()
+    const user = await db('users').first()
+    expect(user).toHaveProperty('id');
+    expect(user).toHaveProperty('username');
+    expect(user).toHaveProperty('password');
   })
 })
